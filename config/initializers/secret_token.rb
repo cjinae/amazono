e9ed025d55dfc726245e575891d8f868f4c94749
Bitmaker Labs <bitmakerlabs@Bitmaker08-2.local>
2013-03-26 20:36:05 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Amazono::Application.config.secret_token = 'e3e457f8f1f6ed9c3b815a91309a76637c46912c269223b5efb967ef4934cbbd6e22bc5ec81c1effde4cc3d989109cad4c0b711fb585e457ada94137f602db76'
