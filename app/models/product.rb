f2628ecaf612bb02438e65b0cb1c3d3eba3c4786
Bitmaker Labs <bitmakerlabs@Bitmaker08-2.local>
2013-03-26 21:13:00 -0400

    Fixed bug with destroy

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :name, :price_in_cents
+
+  validates :name, :presence => true
+  validates :description, :presence => true
+
+
 end
